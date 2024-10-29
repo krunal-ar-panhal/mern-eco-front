553015e335adbd1dc008114422eb09b6bd972670
krunal-ar-panhal <krunalapnchalkp2123@gmail.com>
2024-10-29 18:01:10 +0530

    navbar or routing setup ki navbar or home page ki

--- Desktop/NEXT-JS/projects/mern/client/src/App.jsx
+++ Desktop/NEXT-JS/projects/mern/client/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
